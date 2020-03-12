a89740057e4c2e39d82a21ac203acc40046b7f8e
Pasquale Paudice <pasquale.paudice10@gmail.com>
2020-03-12 10:26:06 +0100

    Fotoooo

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -42,16 +42,15 @@ class ApartmentController extends Controller
     {
         $dati= $request->all();
 
+        $apartment = new Apartment();
+
+        $apartment->fill($dati);
+
         $cover_image= $dati['cover_image'];
 
         $cover_image_path= Storage::put('uploads', $cover_image);
 
-
-        $apartment = new Apartment();
-
         $apartment->cover_image= $cover_image_path;
-
-        $apartment->fill($dati);
 
         $apartment->save();
 
