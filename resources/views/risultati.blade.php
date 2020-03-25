1b91ea08607ee12ef97fd2e069f1658d66cc41f8
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-03-25 15:10:53 +0100

    Cambiato link nell'handlebars per i dettagli

--- resources/views/risultati.blade.php
+++ resources/views/risultati.blade.php
@@ -70,7 +70,7 @@
     </main>
     <script id="template" type="text/x-handlebars-template">
         <div class=" col-xl-3 col-lg-4 col-md-6 mb-4" >
-            <a id="a" href="{{ url('dettagli/')}}/@{{id}}"></a>
+            <a class="vue_card_link" href="{{ url('dettagli/')}}/@{{id}}"></a>
             <div class="bg-white rounded shadow-sm" >
                 <img src="{{ asset('storage/')}}/@{{ cover_image }}" alt="" class="img-fluid card-img-top team-4" >
                 <div class="p-4">
