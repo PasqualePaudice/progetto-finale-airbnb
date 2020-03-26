834643fff361a2e59ef02b758db97da42d15d86c
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-03-26 11:26:44 +0100

    Aggiunti 10 appartamenti Firenze nei seeds

--- config/apartments.php
+++ config/apartments.php
@@ -2,6 +2,7 @@
 
 return
 ['apartment_db' => [
+    // Appartamenti Zona Firenze numero totale: 10
         [
             'title' => 'Appartamento Firenze - Piazza della Signoria',
             'user_id' => '1',
@@ -30,6 +31,119 @@ return
             'cap'=> '50135',
             'visible'=> '1'
         ],
+        [
+            'title' => 'Appartamento Firenze - Zona Franchi',
+            'user_id' => '1',
+            'descrizione_appartamento' => 'Questo delizioso appartamento è un tipico esempio di terra-tetto su 5 livelli e si trova nel comune di Firenze. Necessita di interventi di ristrutturazione ma ha un ottimo potenziale.L\'appartamento si sviluppa su 5 livelli di cui 3 sottostrada con accesso indipendente. Il terzo piano sottostrada è composta da una cantina di circa 20mq, il secondo da una terrazza di 10mq circa, un ripostiglio e una legnaia per circa 25mq, il primo da 1 soggiorno, 1 camera, bagno e ripostiglio per un totale di 37mq. Il piano terreno, con ingresso indipendente dalla parte frontale della casa, è formato da 1 camera, soggiorno, cucina, disimpegno e bagno. Al primo piano troviamo invece due soffitte, per una superficie utile di 48mq circa. Questa proprietà, essendo in un ottima posizione e godendo di vista panoramica, rappresenta una splendida opportunità di ristrutturazione.',
+            'stanze' => '3',
+            'posti_letto' => '2',
+            'bagni'=>'2',
+            'metri_quadri' => '70',
+            'indirizzo'=> 'Via del Campo D\'Arrigo, 15',
+            'state'=> 'Italia',
+            'city'=> 'Firenze',
+            'cap'=> '50131',
+            'visible'=> '1'
+        ],
+        [
+            'title' => 'Appartamento Prato',
+            'user_id' => '1',
+            'descrizione_appartamento' => 'Questo delizioso appartamento è un tipico esempio di terra-tetto su 5 livelli e si trova nel comune di Prato. Necessita di interventi di ristrutturazione ma ha un ottimo potenziale.L\'appartamento si sviluppa su 5 livelli di cui 3 sottostrada con accesso indipendente. Il terzo piano sottostrada è composta da una cantina di circa 20mq, il secondo da una terrazza di 10mq circa, un ripostiglio e una legnaia per circa 25mq, il primo da 1 soggiorno, 1 camera, bagno e ripostiglio per un totale di 37mq. Il piano terreno, con ingresso indipendente dalla parte frontale della casa, è formato da 1 camera, soggiorno, cucina, disimpegno e bagno. Al primo piano troviamo invece due soffitte, per una superficie utile di 48mq circa. Questa proprietà, essendo in un ottima posizione e godendo di vista panoramica, rappresenta una splendida opportunità di ristrutturazione.',
+            'stanze' => '2',
+            'posti_letto' => '1',
+            'bagni'=>'1',
+            'metri_quadri' => '40',
+            'indirizzo'=> 'Viale della Repubblica, 277',
+            'state'=> 'Italia',
+            'city'=> 'Prato',
+            'cap'=> '59100',
+            'visible'=> '1'
+        ],
+        [
+            'title' => 'Appartamento Arezzo',
+            'user_id' => '1',
+            'descrizione_appartamento' => 'Questo delizioso appartamento è un tipico esempio di terra-tetto su 5 livelli e si trova nel comune di Arezzo. Necessita di interventi di ristrutturazione ma ha un ottimo potenziale.L\'appartamento si sviluppa su 5 livelli di cui 3 sottostrada con accesso indipendente. Il terzo piano sottostrada è composta da una cantina di circa 20mq, il secondo da una terrazza di 10mq circa, un ripostiglio e una legnaia per circa 25mq, il primo da 1 soggiorno, 1 camera, bagno e ripostiglio per un totale di 37mq. Il piano terreno, con ingresso indipendente dalla parte frontale della casa, è formato da 1 camera, soggiorno, cucina, disimpegno e bagno. Al primo piano troviamo invece due soffitte, per una superficie utile di 48mq circa. Questa proprietà, essendo in un ottima posizione e godendo di vista panoramica, rappresenta una splendida opportunità di ristrutturazione.',
+            'stanze' => '4',
+            'posti_letto' => '4',
+            'bagni'=>'2',
+            'metri_quadri' => '90',
+            'indirizzo'=> 'Piazza Del Duomo, 1',
+            'state'=> 'Italia',
+            'city'=> 'Arezzo',
+            'cap'=> '52100',
+            'visible'=> '1'
+        ],
+        [
+            'title' => 'Appartamento Empoli',
+            'user_id' => '1',
+            'descrizione_appartamento' => 'Questo delizioso appartamento è un tipico esempio di terra-tetto su 5 livelli e si trova nel comune di Empoli. Necessita di interventi di ristrutturazione ma ha un ottimo potenziale.L\'appartamento si sviluppa su 5 livelli di cui 3 sottostrada con accesso indipendente. Il terzo piano sottostrada è composta da una cantina di circa 20mq, il secondo da una terrazza di 10mq circa, un ripostiglio e una legnaia per circa 25mq, il primo da 1 soggiorno, 1 camera, bagno e ripostiglio per un totale di 37mq. Il piano terreno, con ingresso indipendente dalla parte frontale della casa, è formato da 1 camera, soggiorno, cucina, disimpegno e bagno. Al primo piano troviamo invece due soffitte, per una superficie utile di 48mq circa. Questa proprietà, essendo in un ottima posizione e godendo di vista panoramica, rappresenta una splendida opportunità di ristrutturazione.',
+            'stanze' => '2',
+            'posti_letto' => '1',
+            'bagni'=>'1',
+            'metri_quadri' => '50',
+            'indirizzo'=> 'Viale Palestro',
+            'state'=> 'Italia',
+            'city'=> 'Empoli',
+            'cap'=> '50053',
+            'visible'=> '1'
+        ],
+        [
+            'title' => 'Appartamento Certaldo',
+            'user_id' => '1',
+            'descrizione_appartamento' => 'Questo delizioso appartamento è un tipico esempio di terra-tetto su 5 livelli e si trova nel comune di Certaldo. Necessita di interventi di ristrutturazione ma ha un ottimo potenziale.L\'appartamento si sviluppa su 5 livelli di cui 3 sottostrada con accesso indipendente. Il terzo piano sottostrada è composta da una cantina di circa 20mq, il secondo da una terrazza di 10mq circa, un ripostiglio e una legnaia per circa 25mq, il primo da 1 soggiorno, 1 camera, bagno e ripostiglio per un totale di 37mq. Il piano terreno, con ingresso indipendente dalla parte frontale della casa, è formato da 1 camera, soggiorno, cucina, disimpegno e bagno. Al primo piano troviamo invece due soffitte, per una superficie utile di 48mq circa. Questa proprietà, essendo in un ottima posizione e godendo di vista panoramica, rappresenta una splendida opportunità di ristrutturazione.',
+            'stanze' => '3',
+            'posti_letto' => '2',
+            'bagni'=>'1',
+            'metri_quadri' => '60',
+            'indirizzo'=> 'Via delle Mura, 1',
+            'state'=> 'Italia',
+            'city'=> 'Certaldo',
+            'cap'=> '50052',
+            'visible'=> '1'
+        ],
+        [
+            'title' => 'Appartamento Livorno - Zona Terrazza Mascagni',
+            'user_id' => '1',
+            'descrizione_appartamento' => 'Questo delizioso appartamento è un tipico esempio di terra-tetto su 5 livelli e si trova nel comune di Livorno. Necessita di interventi di ristrutturazione ma ha un ottimo potenziale.L\'appartamento si sviluppa su 5 livelli di cui 3 sottostrada con accesso indipendente. Il terzo piano sottostrada è composta da una cantina di circa 20mq, il secondo da una terrazza di 10mq circa, un ripostiglio e una legnaia per circa 25mq, il primo da 1 soggiorno, 1 camera, bagno e ripostiglio per un totale di 37mq. Il piano terreno, con ingresso indipendente dalla parte frontale della casa, è formato da 1 camera, soggiorno, cucina, disimpegno e bagno. Al primo piano troviamo invece due soffitte, per una superficie utile di 48mq circa. Questa proprietà, essendo in un ottima posizione e godendo di vista panoramica, rappresenta una splendida opportunità di ristrutturazione.',
+            'stanze' => '4',
+            'posti_letto' => '2',
+            'bagni'=>'2',
+            'metri_quadri' => '90',
+            'indirizzo'=> 'Viale Italia, 139',
+            'state'=> 'Italia',
+            'city'=> 'Livorno',
+            'cap'=> '57127',
+            'visible'=> '1'
+        ],
+        [
+            'title' => 'Appartamento Pistoia',
+            'user_id' => '1',
+            'descrizione_appartamento' => 'Questo delizioso appartamento è un tipico esempio di terra-tetto su 5 livelli e si trova nel comune di Pistoia. Necessita di interventi di ristrutturazione ma ha un ottimo potenziale.L\'appartamento si sviluppa su 5 livelli di cui 3 sottostrada con accesso indipendente. Il terzo piano sottostrada è composta da una cantina di circa 20mq, il secondo da una terrazza di 10mq circa, un ripostiglio e una legnaia per circa 25mq, il primo da 1 soggiorno, 1 camera, bagno e ripostiglio per un totale di 37mq. Il piano terreno, con ingresso indipendente dalla parte frontale della casa, è formato da 1 camera, soggiorno, cucina, disimpegno e bagno. Al primo piano troviamo invece due soffitte, per una superficie utile di 48mq circa. Questa proprietà, essendo in un ottima posizione e godendo di vista panoramica, rappresenta una splendida opportunità di ristrutturazione.',
+            'stanze' => '2',
+            'posti_letto' => '1',
+            'bagni'=>'1',
+            'metri_quadri' => '40',
+            'indirizzo'=> 'Piazza del Duomo',
+            'state'=> 'Italia',
+            'city'=> 'Pistoia',
+            'cap'=> '51100',
+            'visible'=> '1'
+        ],
+        [
+            'title' => 'Appartamento Fiesole',
+            'user_id' => '1',
+            'descrizione_appartamento' => 'Questo delizioso appartamento è un tipico esempio di terra-tetto su 5 livelli e si trova nel comune di Firenze. Necessita di interventi di ristrutturazione ma ha un ottimo potenziale.L\'appartamento si sviluppa su 5 livelli di cui 3 sottostrada con accesso indipendente. Il terzo piano sottostrada è composta da una cantina di circa 20mq, il secondo da una terrazza di 10mq circa, un ripostiglio e una legnaia per circa 25mq, il primo da 1 soggiorno, 1 camera, bagno e ripostiglio per un totale di 37mq. Il piano terreno, con ingresso indipendente dalla parte frontale della casa, è formato da 1 camera, soggiorno, cucina, disimpegno e bagno. Al primo piano troviamo invece due soffitte, per una superficie utile di 48mq circa. Questa proprietà, essendo in un ottima posizione e godendo di vista panoramica, rappresenta una splendida opportunità di ristrutturazione.',
+            'stanze' => '3',
+            'posti_letto' => '1',
+            'bagni'=>'1',
+            'metri_quadri' => '60',
+            'indirizzo'=> 'Piazza S. Domenico, 4',
+            'state'=> 'Italia',
+            'city'=> 'Fiesole',
+            'cap'=> '50014',
+            'visible'=> '1'
+        ],
+        // Fine Appartamenti Firenze numero totale: 10
     ]
 ];
 
